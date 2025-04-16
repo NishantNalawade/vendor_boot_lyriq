6eedc37e4283ca98e93930c56072fb13a73328dd
NishantNalawade <1796nishant@gmail.com>
2025-04-17 01:17:59 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -10,11 +10,6 @@ LOCAL_PATH := device/motorola/lyriq
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Virtual A/B
-ENABLE_VIRTUAL_AB := true
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
-
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
